887fe2ac520f09df85753115943c609114cba8d6
Nocivo Cracker <35040649+NocivoCracker@users.noreply.github.com>
2018-01-16 16:59:41 -0200

    Update penboot1.sh

--- penboot1.sh
+++ penboot1.sh
@@ -2,7 +2,7 @@
 
 #cores
 corPadrao="\033[0m"
-preto="\033[0;30m"
+preto="\033[47;1;30m"
 vermelho="\033[0;31m"
 verde="\033[0;32m"
 marrom="\033[0;33m"
@@ -17,45 +17,76 @@ amarelo="\033[1;33m"
 azulClaro="\033[1;34m"
 purpleClaro="\033[1;35m"
 cyanClaro="\033[1;36m"
-branco="\033[1;37m"
+branco="\033[44;1;37m"
+branco2="\033[0;37m"
 
 #Copyright© NocivoCracker 2017 - 2018
 clear
-echo "$azul		_______________________________
-		$amarelo _   _            _            
-		| \ | | ___   ___(_)_   _____  
-		|  \| |/ _ \ / __| \ \ / / _ \ 
-		$verdeClaro| |\  | (_) | (__| |\ V / (_) |
-		|_| \_|\___/ \___|_| \_/ \___/
-$azul		_______________________________"
-echo "
-=============================================================
-$branco criador de pendrive bootavel (necessita modo root)
-$azul============================================================="
-sleep 1.8
-echo "$verde"
-lsblk
+echo "$azul		
+$amarelo _   _            _            
+| \ | | ___   ___(_)_   _____  
+|  \| |/ _ \ / __| \ \ / / _ \ 
+$verdeClaro| |\  | (_) | (__| |\ V / (_) |
+|_| \_|\___/ \___|_| \_/ \___/
+$azul_______________________________
+$preto Copyright© NocivoCracker 2017-2018"
+echo "$verde====================================="
+echo ""
+echo "$branco 1 - criar pendrive bootável"
+echo ""
+echo "$branco 2 - formatar pendrive"
 echo "$azul"
-echo ""
-echo "========================================================================
-$branco digite seu pendrive , geralmende é o $verdeClaro sdb $branco ou o ultimo item da lista :
-$azul========================================================================"
-echo "$cinzaClaro se não sabe qual é o seu pendrive retire ele , reinicie o script e veja o item que falta"
-read pen
-sleep 0.8
-echo "$azul"
-echo "======================================================================"
-echo "$branco Digite o caminho de sua ISO (imagem) de seu sistema desejado :"
-echo "$azul ====================================================================="
-echo "$cinzaClaro ex:/home/user/Downloads/linux.iso"
-read caminho
-sleep 2.0
-echo ""
-echo ""
+read opc
+#menu
+case "$opc" in
+	1)
 clear
-echo "$verde[$vermelho*$verde]$branco criando..."
-echo "$cinzaClaro se o script ficar parado por muito tempo é porque sua iso é pesada e está dando certo , se algo estiver invalido vai aparecer "
-sleep 3.0
-dd if=$caminho of=/dev/$pen bs=4M
+echo "$azul================================================================="
+		echo "$branco criador de pendrive bootavel (necessita modo root)"
+		echo "$azul"
+		sleep 1.8
+		echo "$verde"
+		lsblk
+		echo "$azul"
+		echo ""
+		echo "$branco digite seu pendrive , geralmende é o $verdeClaro sdb $branco ou o ultimo item da lista :"
+		echo "$cinzaClaro se não sabe qual é o seu pendrive retire ele , reinicie o script e veja o item que falta"
+		echo "$azul"
+		read pen
+		sleep 0.8
+		echo "$azul"
+		echo ""
+		echo "$branco Digite o caminho de sua ISO (imagem) de seu sistema desejado :"
+		echo "$azul"
+		echo "$cinzaClaro ex:/home/user/Downloads/linux.iso"
+		echo "$azul"
+		read caminho
+		sleep 2.0
+		echo ""
+		echo ""
+		clear
+echo "$verde[$vermelho*$verde]$branco2 criando..."
+echo "$cinzaClaro se o script ficar parado por muito tempo é porque sua iso é pesada e está dando certo , se algo 	estiver invalido vai aparecer "
+		echo "$branco2"
+		sleep 3.0
+		dd if=$caminho of=/dev/$pen bs=4M status=progress
 echo "$verdeClaro processo terminado"
-
+		;;
+	2)
+clear
+echo "$azul====================================================="
+		echo "$branco Formatar Pendrive"
+		echo "$verde"
+		lsblk
+		echo "$branco Digite o pendrive que deseja formatar"
+echo "$azul"
+		read des
+		sleep 1.8
+		echo "$verde[$vermelho*$verde]$branco2 formatando..."
+		dd if=/dev/zero of=/dev/$des bs=1M status=progress
+		echo "$verde processo terminado"
+		;;
+	*)
+		echo "$vermelho opcao invalida"
+		;;
+esac
